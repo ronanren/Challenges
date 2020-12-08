ce8bedd21de879ab277244f00a5e14c7a1d98b75
ronanren <renouxpro@gmail.com>
2020-12-08 21:09:14 +0100

    update script

--- AdventOfCode2020/7-HandyHaversacks/script1.py
+++ AdventOfCode2020/7-HandyHaversacks/script1.py
@@ -1,21 +1,20 @@
 import sys
 
-lines = []
+lines = {}
 for line in sys.stdin:
-    lines.append(line.replace("\n", ""))
+    lines[line.replace("\n", "").split(" contain ")[0]] = line.replace("\n", "").split(" contain ")[1].split(", ")
 
-bags = {}
-for i in lines:
-    bag = i.split(" contain")[0]
-    bags[bag] = [j.strip() for j in i.split(" contain")[1].split(", ")]
+def recursive(lines, bags, res=[]):
+    tempbag = []
+    
+    for bg, listbag in lines.items():
+        for bag in bags:
+            for j in listbag:   
+                if bag in j and bg not in res:
+                    tempbag.append(bg)
+                    res.append(bg)
+    print(len(res))
+    if tempbag != []:
+        recursive(lines, tempbag, res)
 
-
-def recursive(bags, my_bag="shiny gold", array=[]):
-    for key, value in bags.items():
-        for j in value:
-            if my_bag in j and key not in array:
-                array.append(key)          
-                recursive(bags, my_bag=key)
-    return array
-
-print(len(recursive(bags)))
+recursive(lines, ['shiny gold'])
